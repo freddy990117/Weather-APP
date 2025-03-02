38da27c0b7229a289d2cea2485fbb07032acbd46
Freddy990117 <Freddy990117@gmail.com>
2025-03-02 20:47:06 +0800

    按下 Enter 會執行 fetch Data

--- app.js
+++ app.js
@@ -13,11 +13,61 @@ const humidity = document.querySelector(".weather-Details .humidity span");
 const wind = document.querySelector(".weather-Details .wind span");
 
 // 按下輸入鍵時時開始
+// search.addEventListener("click", () => {
+//   const APIKey = "d4f28db3d2a09114f45439c6fb382b9a";
+//   const city = document.querySelector(".search-box input").value;
+//   fetch(
+//     `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
+//   ).then((response) => {
+//     response.json().then((result) => {
+//       if (result.cod === "404") {
+//         // 設定 Error 404 的 style
+//         container.style.height = "405px";
+//         error404.style.display = "block";
+//         error404.classList.add("fadeIn");
+//       } else {
+//         // 設定 cod 200 的 style
+//         container.style.height = "605px";
+//         weatherBox.classList.add("fadeIn");
+//         weatherBox.style.display = "block";
+//         weatherDetails.classList.add("fadeIn");
+//         error404.style.display = "none";
+//         // 設定得到數值並 取代/顯示 在 HTNL 上
+//         temperature.innerHTML = `${parseInt(result.main.temp)}°Ｃ`;
+//         description.innerHTML = `${result.weather[0].description}`;
+//         humidity.innerHTML = `${result.main.humidity}%`;
+//         wind.innerHTML = `${result.wind.speed}km/h`;
+//         // 根據天氣顯示相應的圖片（此情境較適合使用 Switch case（簡單的情境） ）
+//         switch (result.weather[0].main) {
+//           case "Clear":
+//             image.src = "./images/clear.png";
+//             break;
+//           case "Rain":
+//             image.src = "./images/rain.png";
+//             break;
+//           case "Snow":
+//             image.src = "./images/mist.png";
+//             break;
+//           case "Haze":
+//             image.src = "./images/snow.png";
+//             break;
+//           case "Clouds":
+//             image.src = "./images/cloud.png";
+//             break;
+//           // 當 expression 的值都不符合上述條件
+//           default:
+//             image.src = "";
+//             break;
+//         }
+//       }
+//     });
+//   });
+// });
 
-search.addEventListener("click", () => {
-  const APIKey = "d4f28db3d2a09114f45439c6fb382b9a";
+// 1.先將 fetch Data 分割出來
+const fetchWeather = () => {
   const city = document.querySelector(".search-box input").value;
-
+  const APIKey = "d4f28db3d2a09114f45439c6fb382b9a";
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
   ).then((response) => {
@@ -27,20 +77,21 @@ search.addEventListener("click", () => {
         container.style.height = "405px";
         error404.style.display = "block";
         error404.classList.add("fadeIn");
+        weatherBox.classList.remove("fadeIn");
+        weatherDetails.classList.remove("fadeIn");
       } else {
         // 設定 cod 200 的 style
         container.style.height = "605px";
         weatherBox.classList.add("fadeIn");
-        weatherBox.style.display = "block";
         weatherDetails.classList.add("fadeIn");
+        weatherBox.style.display = "block";
         error404.style.display = "none";
-
+        error404.classList.remove("fadeIn");
         // 設定得到數值並 取代/顯示 在 HTNL 上
         temperature.innerHTML = `${parseInt(result.main.temp)}°Ｃ`;
         description.innerHTML = `${result.weather[0].description}`;
         humidity.innerHTML = `${result.main.humidity}%`;
         wind.innerHTML = `${result.wind.speed}km/h`;
-
         // 根據天氣顯示相應的圖片（此情境較適合使用 Switch case（簡單的情境） ）
         switch (result.weather[0].main) {
           case "Clear":
@@ -58,7 +109,6 @@ search.addEventListener("click", () => {
           case "Clouds":
             image.src = "./images/cloud.png";
             break;
-
           // 當 expression 的值都不符合上述條件
           default:
             image.src = "";
@@ -67,16 +117,17 @@ search.addEventListener("click", () => {
       }
     });
   });
-});
-
-// 按下 Enter 鍵後執行後執行搜尋
-const keyDown = () => {
-  addEventListener("keydown", (e) => {
-    console.log(e.key);
-    if (e.key === "Enter") {
-      console.log(e);
-    }
-  });
 };
 
-keyDown();
+// 2.按下 Enter 後執行 fetch Data
+const input = document.querySelector(".search-box input");
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    fetchWeather();
+  }
+  ;
+});
+// 3.點選搜尋後執行 fetch Data
+search.addEventListener("click", () => {
+  fetchWeather();
+});
