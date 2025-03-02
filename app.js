3f60d95c00dff0733d6b5101a1f8551b383b0ab9
Freddy990117 <Freddy990117@gmail.com>
2025-03-02 20:14:45 +0800

    Code Finish ~ (但沒辦法按下 Enter 後搜尋

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// class name
 const container = document.querySelector(".container");
 const search = document.querySelector(".search-box button");
 const weatherBox = document.querySelector(".weather-box");
@@ -11,63 +12,71 @@ const description = document.querySelector(".weather-box .description");
 const humidity = document.querySelector(".weather-Details .humidity span");
 const wind = document.querySelector(".weather-Details .wind span");
 
-// 追蹤輸入的數值並在按下時開始
+// 按下輸入鍵時時開始
+
 search.addEventListener("click", () => {
-  const city = document.querySelector(".search-box input").value;
-  console.log(city);
   const APIKey = "d4f28db3d2a09114f45439c6fb382b9a";
+  const city = document.querySelector(".search-box input").value;
 
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
-  )
-    // fetch 出一個 Proimes
-    .then((response) => response.json())
-    // 取出資料
-    .then((result) => {
-      // 如果出現錯誤
+  ).then((response) => {
+    response.json().then((result) => {
       if (result.cod === "404") {
-        // 高度變成 500 並加入顯示動畫
-        container.style.height = "500px";
+        // 設定 Error 404 的 style
+        container.style.height = "405px";
         error404.style.display = "block";
         error404.classList.add("fadeIn");
+      } else {
+        // 設定 cod 200 的 style
+        container.style.height = "605px";
+        weatherBox.classList.add("fadeIn");
+        weatherBox.style.display = "block";
+        weatherDetails.classList.add("fadeIn");
+        error404.style.display = "none";
+
+        // 設定得到數值並 取代/顯示 在 HTNL 上
+        temperature.innerHTML = `${parseInt(result.main.temp)}°Ｃ`;
+        description.innerHTML = `${result.weather[0].description}`;
+        humidity.innerHTML = `${result.main.humidity}%`;
+        wind.innerHTML = `${result.wind.speed}km/h`;
+
+        // 根據天氣顯示相應的圖片（此情境較適合使用 Switch case（簡單的情境） ）
+        switch (result.weather[0].main) {
+          case "Clear":
+            image.src = "./images/clear.png";
+            break;
+          case "Rain":
+            image.src = "./images/rain.png";
+            break;
+          case "Snow":
+            image.src = "./images/mist.png";
+            break;
+          case "Haze":
+            image.src = "./images/snow.png";
+            break;
+          case "Clouds":
+            image.src = "./images/cloud.png";
+            break;
+
+          // 當 expression 的值都不符合上述條件
+          default:
+            image.src = "";
+            break;
+        }
       }
-      // 沒有出現以上錯誤就執行：
-      //   選擇使用 switch 原因是因為狀況較簡單，不用使用到 if else
-      switch (result.weather[0].main) {
-        case "Clear":
-          image.src = "images/clear.png";
-          break;
-
-        case "Rain":
-          image.src = "images/rain.png";
-          break;
-
-        case "Snow":
-          image.src = "images/snow.png";
-          break;
-
-        case "Clouds":
-          image.src = "images/cloud.png";
-          break;
-
-        case "Haze":
-          image.src = "images/mist.png";
-          break;
-
-        default:
-          image.src = "";
-      }
-
-      temperature.innerHTML = `${parseInt(result.main.temp)}<span>°C</span>`;
-      description.innerHTML = `${result.weather[0].description}`;
-      humidity.innerHTML = `${result.main.humidity}%`;
-      wind.innerHTML = `${parseInt(result.wind.speed)}Km/h`;
-
-      console.log(result);
     });
+  });
 });
 
-// name
-// main.humidity
-// wind.speed
-// main.temp
+// 按下 Enter 鍵後執行後執行搜尋
+const keyDown = () => {
+  addEventListener("keydown", (e) => {
+    console.log(e.key);
+    if (e.key === "Enter") {
+      console.log(e);
+    }
+  });
+};
+
+keyDown();
